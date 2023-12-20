b49f92e744ee624daf6e429e506b89c7e11d260d
ggghost <826842941@qq.com>
2023-12-20 22:19:20 +0800

    ggghost-admin-react create

---
+++ ggghost-captcha/src/main/java/com/ggghost/captcha/App.java
@@ -0,0 +1,12 @@
+package com.ggghost.captcha;
+
+/**
+ * @author ggghost
+ * create 2023/12/20 15:35
+ * @Description
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("==============");
+    }
+}
